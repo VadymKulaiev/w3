1a7213a2d04a9be4f0c83ff96bd106863bb00050
Vadym <Kylaev1805@gmail.com>
2024-01-04 23:12:37 +0200

    weather

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
-import { useState } from 'react';
 
 function App() {
 
-  const[color, setColor] = useState({color: getRandomColor()});
+  const [data, setData] = useState({});
+  const [city, setCity] = useState('');
 
-  const changeColor = () => {
-    setColor({color: getRandomColor()});
+  const key = '2f5b187c31444642d9d3dc99178ec345';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}&lang=ua`;
+
+  useEffect(() => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=kyiv&units=metric&appid=${key}&lang=ua`)
+    .then(res => setData(res.data))
+    .catch(err => console.log(err));
+  }, [])
+
+  const onSearchWeather = (e) => {
+    if (e.key === 'Enter') {
+      axios.get(url)
+        .then(response => setData(response.data))
+        .catch(err => console.log(err))
+      setCity('');
+    }
   }
 
-  function getRandomColor() {
-    let r = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-    let g = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-    let b = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-    return `rgb(${r}, ${g}, ${b})`;
-  }
+  return (
+    <div className='app'>
+      <div className='inp-field'>
+        <input type='text' placeholder='Enter Location'
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyDown={onSearchWeather}
+        />
+      </div>
 
-  return(
-    <>
-    <p style={color}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam praesentium doloribus quidem vitae blanditiis iste nemo aut, eum deserunt, deleniti veritatis labore nobis, dicta eveniet omnis veniam earum impedit incidunt!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium numquam ipsum excepturi ullam, eligendi in. Ipsa, debitis? Perspiciatis id iure exercitationem perferendis officiis beatae repudiandae atque, expedita numquam itaque labore.</p>
-    <button onClick={changeColor}>Зміна кольору</button>
-    </>
+      <div className='container'>
+        {data.name !== undefined && (
+          <>
+            <div className='header'>
+              <div className='city'>
+                <p>{data.name}</p>
+              </div>
+            </div>
+            <div className='temp'>
+              {data.main && <h1>{data.main.temp.toFixed()}°C</h1>}
+            </div>
+            <div className='minmax'>
+              <div className='min'>
+              <p>{data.main.temp_min.toFixed()}°C</p>
+              <p>мін</p>
+              </div>
+              <div className='max'>
+              <p>{data.main.temp_max.toFixed()}°C</p>
+              <p>мах</p>
+              </div>
+            </div>
+            <div className='desc'>
+              {data.weather ? <p>{data.weather[0].description}</p> : null}
+            </div>
+            <div className='footer'>
+              <div className='feels'>
+                {data.main ? <p className='bolt'>{data.main.feels_like.toFixed()}°C</p> : null}
+                <p>Відчувається як </p>
+              </div>
+              <div className='humidity'>
+                {data.main && <p className='bolt'> {data.main.humidity}%</p>}
+                <p>Вологість</p>
+              </div>
+              <div className='wind'>
+                {data.wind && <p className='bolt'> {data.wind.speed} м/с</p>}
+                <p>Вітер</p>
+              </div> 
+             
+             
+            </div>
+            <div className='footer'>
+            <div className='pressure'>
+                {data.main && <p className='bolt'>Тиск {data.main.pressure} мм</p>}
+              </div>
+            </div>
+
+          </>
+        )}
+
+
+
+      </div>
+    </div>
   )
 }
 
